e830d3e5eaf8a3710470c07c1d384fbc6dfd59b5
MauricioPujol <mauricio.pujolh@gmail.com>
2023-09-10 21:01:00 -0300

    Changed main script to use Selenium instead of bs

--- selenium_main.py
+++ selenium_main.py
@@ -1,19 +1,32 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from helpers import *
+import time
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-import pandas as pd
-import re
-from datetime import datetime
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from helpers import *
-import time
+browser = webdriver.Firefox()
 # This is my profile containing all read and unread books
 url = r'https://www.goodreads.com/review/list/142380450-mauricio-pujol?utf8=%E2%9C%93&shelf=%23ALL%23&per_page=infinite'
-page = urlopen(url)
-html = page.read().decode("utf-8")
+browser.get(url)
+time.sleep(2)
+
+scroll_pause_seconds = 1
+# Get scroll height
+last_height = browser.execute_script("return document.body.scrollHeight")
+while True:
+    # Scroll down to bottom
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load page
+    time.sleep(scroll_pause_seconds)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = browser.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+html = browser.page_source
 soup = BeautifulSoup(html, "html.parser")
 html_tr = soup.find_all("tr")
 df_my_books = pd.DataFrame({'title': pd.Series(dtype='str'),  # This defines the structure for the dataframe to store scraped data
@@ -34,8 +47,8 @@ for i in range(0, len(html_tr)):
             html_tr[i].find('td', {'class': 'field author'}
                             ).a.text.strip(),  # Extract book author
             # Extract book number of pages
-            int(re.search(r'[\d]+', html_tr[i].find('td',
-                {'class': 'field num_pages'}).text).group()),
+            get_number_of_pages(html_tr[i].find('td',
+                                                {'class': 'field num_pages'}).text),
             # Extract average rating
             float(re.search(r'[\d+\.\d+]+', html_tr[i].find('td',
                   {'class': 'field avg_rating'}).text).group()),
@@ -50,44 +63,13 @@ for i in range(0, len(html_tr)):
             # html_tr[i].find('td',{'class':'field title'}).a.text.strip(),
         ]
     except:
+        print('Book', i, 'not found')
         pass
 # Add a column to determine if book was read
 df_my_books['is_read'] = df_my_books['rating'] >= 0
+
 url_prefix = 'https://www.goodreads.com'
 df_books_details = pd.DataFrame({'book_description': pd.Series(dtype='str'),
                                  'author_description': pd.Series(dtype='str'),
                                  'genres': pd.Series(dtype='str')
                                  })
-
-for title, book_url in df_my_books[['title', 'cover_link']].values:
-    time.sleep(1)
-    page = urlopen(url_prefix+book_url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    html_div = soup.find_all(
-        'div', {'class': 'DetailsLayoutRightParagraph__widthConstrained'})
-    # debug start
-    # pretty_soup = soup.prettify()
-    # with open(title[0:3]+".txt","w") as out:
-    #    for i in range(0, len(pretty_soup)):
-    #        try:
-    #            out.write(pretty_soup[i])
-    #        except:
-    #            pass
-    # print()
-    # debug end
-    book_description = html_div[0].text
-    author_description = html_div[1].text
-    try:
-        genres_str = ''
-        genres_soup = list(
-            soup.find('ul', {'class': 'CollapsableList', "aria-label": True}).span)
-        for genre in genres_soup:
-            if genre.text != 'Genres':
-                genres_str = genres_str + genre.text + ', '
-    except:
-        genres_str = None
-    df_books_details.loc[len(df_books_details.index)] = [
-        book_description, author_description, genres_str]
-
-df_my_books
